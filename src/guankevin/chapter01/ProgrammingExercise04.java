e1ee27d47174c70de281de4857e2d298b4318e32
Kevin Guan <kevguan@yahoo.com>
2018-11-10 16:39:36 -0600

    Completed Chaper 01 Programming Exercise 04

---
+++ src/guankevin/chapter01/ProgrammingExercise04.java
@@ -0,0 +1,31 @@
+package guankevin.chapter01;
+
+/**
+ * Introduction to Java Programming and Data Structures 11th Edition by Daniel Liang.
+ * Chapter 01 Introduction to Computers, Programs and Java
+ * Programming Exercise 04 - Print a Table
+ *
+ * @author guankevin
+ * @version 1.0
+ * @since 2018-11-10
+ */
+public class ProgrammingExercise04 {
+    public static void main(String[] args) {
+        /*
+            Write a program that displays the following table:
+            a        a^2        a^3
+            1        1        1
+            2        4        8
+            3        9        27
+            4        16        64
+         */
+
+        // \t escape sequence for a tab space
+
+        System.out.println("a\t\ta^2\t\ta^3");
+        System.out.println("1\t\t1\t\t1");
+        System.out.println("2\t\t4\t\t8");
+        System.out.println("3\t\t9\t\t27");
+        System.out.println("4\t\t16\t\t64 ");
+    }
+}
