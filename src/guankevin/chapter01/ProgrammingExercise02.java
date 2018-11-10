37ce0264d2d7c34bb301e818e0f1f6a56c30862b
Kevin Guan <kevguan@yahoo.com>
2018-11-10 15:54:24 -0600

    "Added Programming exercise 02"

---
+++ src/guankevin/chapter01/ProgrammingExercise02.java
@@ -0,0 +1,27 @@
+package guankevin.chapter01;
+import java.lang.System;
+
+/**
+ * Introduction to Java Programming and Data Structures 11th Edition by Daniel Liang.
+ * Chapter 01 Introduction to Computers, Programs and Java
+ * Programming Exercise 02 - Display Five Messages
+ *
+ * @author guankevin
+ * @version 1.0
+ * @since 2018-11-10
+ */
+public class ProgrammingExercise02 {
+    public static void main(String[] args) {
+        // Write a program that displays “Welcome to Java” five times.
+        System.out.print("" +
+                "Welcome to Java  \n" +
+                "Welcome to Java\n" +
+                "Welcome to Java \n" +
+                "Welcome to Java\n" +
+                "Welcome to Java");
+
+        // System.out.println("Welcome to Java");
+    }
+}
+
+
