0571e4bcaf434c3a1a17fc8dfae625daad9e7983
Kevin Guan <kevguan@yahoo.com>
2018-11-10 17:20:00 -0600

    Completed Chapter 01 Programming Exercise 06

---
+++ src/guankevin/chapter01/ProgrammingExercise06.java
@@ -0,0 +1,21 @@
+package guankevin.chapter01;
+
+/**
+ * Introduction to Java Programming and Data Structures 11th Edition by Daniel Liang.
+ * Chapter 01 Introduction to Computers, Programs and Java
+ * Programming Exercise 06 - Summation of a series
+ *
+ * @author guankevin
+ * @version 1.0
+ * @since 2018-11-10
+ */
+public class ProgrammingExercise06 {
+    public static void main(String[] args) {
+        /*
+            1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9
+         */
+
+        System.out.println("1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 = " +
+                (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9));
+    }
+}
