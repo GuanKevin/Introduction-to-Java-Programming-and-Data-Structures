c772d87d938643689dd35d63dcb1c4e9bd9a1aa9
Kevin Guan <kevguan@yahoo.com>
2018-11-10 16:57:49 -0600

    Completed Chapter 01 Programming Exercise 05

---
+++ src/guankevin/chapter01/ProgrammingExercise05.java
@@ -0,0 +1,22 @@
+package guankevin.chapter01;
+
+/**
+ * Introduction to Java Programming and Data Structures 11th Edition by Daniel Liang.
+ * Chapter 01 Introduction to Computers, Programs and Java
+ * Programming Exercise 05 - Compute Expressions
+ *
+ * @author guankevin
+ * @version 1.0
+ * @since 2018-11-10
+ */
+public class ProgrammingExercise05 {
+    public static void main(String[] args) {
+        /*
+            Write a program that displays the result of
+            9.5 x 4.5 - 2.5 x 345.5 - 3.5
+         */
+
+        //System.out.println(9.5 * 4.5 - 2.5 * 345.5 - 3.5);
+        System.out.println("9.5 x 4.5 - 2.5 x 345.5 - 3.5 = " + (2.5 * 345.5 - 9.5 * 4.5 - 3.5));
+    }
+}
