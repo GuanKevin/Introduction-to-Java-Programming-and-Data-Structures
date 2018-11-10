5581dcb84c4176f048f832023d3430f9da4cd748
Kevin Guan <kevguan@yahoo.com>
2018-11-10 17:47:52 -0600

    Completed Chapter 01 Exercise 08

---
+++ src/guankevin/chapter01/ProgrammingExercise08.java
@@ -0,0 +1,22 @@
+package guankevin.chapter01;
+
+/**
+ * Introduction to Java Programming and Data Structures 11th Edition by Daniel Liang.
+ * Chapter 01 Introduction to Computers, Programs and Java
+ * Programming Exercise 08
+ *
+ * @author guankevin
+ * @version 1.0
+ * @since
+ */
+public class ProgrammingExercise08 {
+    public static void main(String[] args) {
+        /*
+            Write a program that displays the area and perimeter of a
+            circle that has a radius of 5.5 using the following formulas:
+
+            Perimeter = 2 x radius x pi
+            Area = radius x radius x pi
+         */
+    }
+}
