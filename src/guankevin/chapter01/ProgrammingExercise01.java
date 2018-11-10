0013a0061150c30a0ad5468888e91d4f91e5e8de
Kevin Guan <kevguan@yahoo.com>
2018-11-09 18:40:36 -0600

    Completed Chapter 01 Programming Exercise 01

---
+++ src/guankevin/chapter01/ProgrammingExercise01.java
@@ -0,0 +1,25 @@
+package guankevin.chapter01;
+import java.lang.System;
+
+/**
+ * Introduction to Java Programming and Data Structures 11th Edition by Daniel Liang.
+ * Chapter 01 Introduction to Computers, Programs and Java
+ * Programming Exercise 1
+ *
+ * @author guankevin
+ * @version 1.0
+ * @since 2018-11-09
+ */
+public class ProgrammingExercise01 {
+    // Main method - Java Entry Point
+    public static void main(String[] args) {
+        // Step 1: Display - Welcome to Java
+        System.out.println("Welcome to Java");
+
+        // Step 2: Display - Welcome to Computer Science
+        System.out.println("Welcome to Computer Science");
+
+        // Step 3: Display - Programming is fun
+        System.out.print("Programming is fun");
+    }
+}
