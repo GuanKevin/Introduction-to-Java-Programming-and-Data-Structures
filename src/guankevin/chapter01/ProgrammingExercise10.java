af79644326fdf9bd3e36bed6f8b38e5a2eb1f0a0
Kevin Guan <kevguan@yahoo.com>
2018-11-10 18:40:02 -0600

    Completed Chapter 01 Programming Exercise 10

---
+++ src/guankevin/chapter01/ProgrammingExercise10.java
@@ -0,0 +1,24 @@
+package guankevin.chapter01;
+
+/**
+ * Introduction to Java Programming and Data Structures 11th Edition by Daniel Liang.
+ * Chapter 01 Introduction to Computers, Programs and Java
+ * Programming Exercise 10 Average Speed in Miles
+ *
+ * @author guankevin
+ * @version 1.0
+ * @since 2018-11-10
+ */
+public class ProgrammingExercise10 {
+    public static void main(String[] args) {
+        /*
+            Assume that a runner runs 14 kilometers in 45 minutes and 30 seconds.
+            Write a program that displays the average speed in miles per hour.
+            (Note 1 mile is equal to 1.6 kilometers.)
+         */
+
+        System.out.println("14 kilometers to miles is " + (14 / 1.6) + ".");
+        System.out.println("45 minutes and 30 seconds to hour is " + (45.5 / 60) + ".");
+        System.out.println("Runner runs " + (8.75 / 0.7583333333333333) + " miles per hour.");
+    }
+}
